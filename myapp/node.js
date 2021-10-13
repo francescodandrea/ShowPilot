07f04207a65f1cdf0b19b8c0c75fa11584db5d61
Francesco D'Andrea <francescodandrea339@gmail.com>
2021-10-13 11:56:49 +0200

    easymidi

--- myapp/node.js
+++ myapp/node.js
@@ -1,9 +1,16 @@
 const express = require('express')
+var easymidi = require('easymidi');
+
 const app = express();
 const port = 8000;
 
+var inputs = easymidi.getInputs();
+var outputs = easymidi.getOutputs();
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
+  console.log(inputs);
+  console.log(outputs);
 });
 
 app.listen(port, () => {
