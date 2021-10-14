f77c6a8b647d887d3007b00c57e01710d27656db
fra <francescodandrea339@gmail.com>
2021-10-14 12:44:38 +0200

    testing dashboard bootstrap setup

--- myapp/static/testing/midiCom.js
+++ myapp/static/testing/midiCom.js
@@ -1,1 +1,11 @@
 //Midi communication
+function sendmidi(){
+    var xhr = new XMLHttpRequest();
+        xhr.addEventListener("readystatechange", function() {
+            if (this.readyState === this.DONE) {
+                    var result=JSON.parse(this.responseText);
+                }
+        });
+        xhr.open("GET", "");
+        xhr.send();
+}
