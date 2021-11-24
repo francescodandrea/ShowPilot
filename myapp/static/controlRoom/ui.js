9906fcb4f71dcacb382d232e93fb746494dd7484
francescodandrea <francescodandrea339@gmail.com>
2021-11-24 17:58:35 +0100

    reorganized

--- myapp/static/controlRoom/ui.js
+++ myapp/static/controlRoom/ui.js
@@ -1,5 +1,5 @@
 //Ui js
-var opensection="scenes";
+var opensection="devices"; //startup screen
 document.querySelector("#"+opensection).style.display="inherit";
 document.querySelector("#"+opensection).style.opacity=1;
 
@@ -15,4 +15,65 @@ function section(x){
             document.querySelector("#"+prev).style.display="none";
         }, 500);
     }
+}
+
+//devices upd
+//----set get
+function devicestoselect(result){
+    //set
+    let inlist = document.querySelector("#inputGroupSelect01");
+    let outlist = document.querySelector("#inputGroupSelect02");
+
+    //setup
+    inlist.innerHTML="";
+    outlist.innerHTML="";
+
+    result.inputs.forEach(element => {
+        var option = document.createElement("option");
+        option.innerHTML=element;
+        inlist.appendChild(option);
+    });
+
+    result.outputs.forEach(element => {
+        var option = document.createElement("option");
+        option.innerHTML=element;
+        outlist.appendChild(option);
+    });
+}
+function setdevices(){
+    let inp = document.querySelector("#inputGroupSelect01");
+    let out = document.querySelector("#inputGroupSelect02");
+    let inptxt; let outtxt;
+    try {
+        inptxt = inp.options[inp.selectedIndex].text;
+    } catch (error) {
+        if(inptxt==undefined){inptxt="undefined";}
+    }
+    try {
+        outtxt = out.options[out.selectedIndex].text;
+    } catch (error) {
+        if(outtxt==undefined){outtxt="undefined";}
+    }
+    
+    senddevices(inptxt, outtxt);
+}
+//----devices status
+function statusupd(element, bool){
+    let arg = document.querySelector("#status_"+element);
+        arg.className="status";
+    setTimeout(() => {
+        if(bool){
+            arg.className="status ok";
+        } else {
+            arg.className="status no";
+        }
+    }, 200);
+}
+
+//send cc
+function sendccui(){
+    let ch = document.querySelector("#channel").value;
+    let co = document.querySelector("#controller").value;
+    let va = document.querySelector("#value").value;
+    sendcc(ch,co,va);
 }
