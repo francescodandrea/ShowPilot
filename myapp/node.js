36f3b1e0795310c6700b69c8f6e18a292af21159
francescodandrea <francescodandrea339@gmail.com>
2021-11-24 17:56:55 +0100

    methods reorganized

--- myapp/node.js
+++ myapp/node.js
@@ -10,6 +10,9 @@ app.use(
     origin: "*",
   })
 )
+app.use('/static', express.static('public'));
+
+//SERVER STARTUP
 
 var inputs = easymidi.getInputs();
 var outputs = easymidi.getOutputs();
@@ -17,25 +20,25 @@ var outputs = easymidi.getOutputs();
 var myinput=false;
 var myoutput=false;
 
-app.use('/static', express.static('public'));
+//SETTINGS
 
-app.get('/testdevices', (req, res) => {
+//SETUP COMMUNICATION
+app.get('/devices', (req, res) => {
   console.log(`Getting devices`);
-  var inputs = easymidi.getInputs();
-  var outputs = easymidi.getOutputs();
+  let inputs = easymidi.getInputs();
+  let outputs = easymidi.getOutputs();
   res.json({ "inputs": inputs, "outputs": outputs });
 });
-
-app.put('/setdevices', (req, res) => {
+app.post('/devices', (req, res) => {
   if(req.query.in!="undefined")
     myinput = new easymidi.Input(req.query.in);
   if(req.query.out!="undefined")
     myoutput = new easymidi.Output(req.query.out);
-
   console.log('Devices set:'+ req.query.in+", "+req.query.out);
   res.json({ "log": "Devices set on server", "com": {"in": req.query.in, "out": req.query.out} });
 });
 
+//TO MIDI
 app.post('/sendcc', (req, res) => {
   console.log(req.query.channel+" "+req.query.controller+" "+req.query.value)
   
@@ -48,31 +51,38 @@ app.post('/sendcc', (req, res) => {
   res.json({ "log": "CC sent "+req.query.channel+' '+req.query.controller+' '+req.query.value, "com": {"in": req.query.in, "out": req.query.out} });
 });
 
+//FROM MIDI
+
+//testing only
+app.get('/testdevices', (req, res) => {
+  console.log(`Getting devices`);
+  var inputs = easymidi.getInputs();
+  var outputs = easymidi.getOutputs();
+  res.json({ "inputs": inputs, "outputs": outputs });
+});
+app.put('/setdevices', (req, res) => {
+  if(req.query.in!="undefined")
+    myinput = new easymidi.Input(req.query.in);
+  if(req.query.out!="undefined")
+    myoutput = new easymidi.Output(req.query.out);
+
+  console.log('Devices set:'+ req.query.in+", "+req.query.out);
+  res.json({ "log": "Devices set on server", "com": {"in": req.query.in, "out": req.query.out} });
+});
 app.get('/ping', (req, res) => {
   console.log(`Pong`);
   res.json({ state: 'Pong' })
 });
-
-app.get('/testrecive', (req, res) => {
-  console.log(`Reciving`);
-  res.json({ state: 'Recived', note: 100 })
-});
-
 app.get('/controlRoom', (req, res) => {
   res.sendFile('static/controlRoom/index.html', {root: __dirname })
 });
-
 app.get('/testing', (req, res) => {
   res.sendFile('static/testing/index.html', {root: __dirname })
 });
 
-app.get('/', (req, res) => {
-  console.log(inputs);
-  console.log(outputs);
-});
-
+//LISTEN PORT
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+  console.log(`Example app listening on port ${port}`)
 });
 
 //npm start
