0cc8e54fd852713a1733337821092fdf13d2d594
francescodandrea <francescodandrea339@gmail.com>
2022-02-22 09:31:46 +0100

    test fix

--- glogged.php
+++ glogged.php
@@ -1,12 +1,12 @@
 <?php
 if(isset($_GET["code"])){
-    $token=$client->fetchAccessTokenwithAuthCode ($_GET['code']);
+    $token=$client->fetchAccessTokenwithAuthCode($_GET['code']);
 
     $client->setAccessToken ($token);
 
     //Getting User Profile Sgauth-new Google Service 1
 
-    Oauth2($client);
+    $google_info=Oauth2($client);
 
     $google_info=$gauth->userinfo->get();
     $email = $google_info->email;
