b8c26d3881890d4154354c1e80f2ac7c5acd651f
francescodandrea <francescodandrea339@gmail.com>
2022-02-22 09:45:55 +0100

    fix

--- glogged.php
+++ glogged.php
@@ -5,11 +5,11 @@ if(isset($_GET["code"])){
     $token=$client->fetchAccessTokenwithAuthCode($_GET['code']);
 
     $client->setAccessToken ($token);
-/*
+
     //Getting User Profile Sgauth-new Google Service 1
 
     $gauth=Oauth2($client);
-
+/*
     $google_info=$gauth->userinfo->get();
     $email = $google_info->email;
     $name = $google_info->name;
