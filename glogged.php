9d2ac71b4084791db0fd69a78f59daecc2e7e880
francescodandrea <francescodandrea339@gmail.com>
2022-02-22 09:50:49 +0100

    fix

--- glogged.php
+++ glogged.php
@@ -12,6 +12,7 @@ if(isset($_GET["code"])){
 
     $google_info=$gauth->userinfo->get();
     $email = $google_info->email;
+    echo $email;
 /*
     $name = $google_info->name;
     echo "Welcome ". $name.". You are registered using email: ".$email;
