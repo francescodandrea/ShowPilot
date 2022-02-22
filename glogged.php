d6e462f753ecca24259000d79f14c105f1095b25
francescodandrea <francescodandrea339@gmail.com>
2022-02-22 09:24:36 +0100

    user info

--- glogged.php
+++ glogged.php
@@ -1,3 +1,21 @@
 <?php
-echo "hi"
+if(isset($_GET["code"])){
+    $token-$client->fetchAccessTokenwithAuthCode ($_GET['code']);
+
+    $client->setAccessToken ($token);
+
+    //Getting User Profile Sgauth-new Google Service 1
+
+    Oauth2($client);
+
+    $google_info=$gauth->userinfo->get();
+    $email = $google_info->email;
+    $name = $google_info->name;
+    echo "Welcome ". $name.". You are registered using email: ".$email;
+
+} else {
+    echo "<a href=". $client->createAuthurl().">Login with Google</a>";
+}
+
+
 ?>
