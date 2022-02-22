57846df4d2235b33d4b308705adc87a04311431b
francescodandrea <56073893+francescodandrea@users.noreply.github.com>
2022-02-22 06:56:07 +0100

    500 fixing test

--- rest/Gauth.php
+++ rest/Gauth.php
@@ -9,7 +9,7 @@ var_dump($gouthSecrets);
 */
 $clientID = '211776424215-gollvp5dbmplhdcualakcajfdcrrqp7e.apps.googleusercontent.com';
 $clientSecret = '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';
-$redirectUrl = 'https://showpilot.herokuapp.com/account';
+$redirectUrl = 'https://showpilot.herokuapp.com/glogged';
 
 // Creating client request to google
 
