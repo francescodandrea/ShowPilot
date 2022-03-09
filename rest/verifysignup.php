a7969dc65203b35b18ab7b3436626c24d114f0a3
Francesco D'Andrea <francescodandrea339@gmail.com>
2022-03-09 12:14:34 +0100

    test

--- rest/verifysignup.php
+++ rest/verifysignup.php
@@ -16,13 +16,12 @@ $opts = array('http' =>
 );
 $context = stream_context_create($opts);
 $result = file_get_contents('https://francescodandreastudente.altervista.org/showPilotREST/signup', false, $context);
-echo $result;
 
 if($result["result"]){
-    require ('page.php');
+    require ('../page.php');
 }{
     $sigerr=$result["message"];
-    require ('account.php');
+    require ('../account.php');
 }
 
 ?>
